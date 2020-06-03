1a14e67721de274260060a500b9319f6e66eb7d5
Scott Wakefield <scott@expertcc.co.uk>
2020-06-03 16:50:46 +0100

    FAT2-80 adding initial part of standards controller.

---
+++ src/SFA.DAS.Courses.Api.UnitTests/Models/WhenCastingToGetStandardResponseFromDomainType.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Courses.Api.ApiResponses;
+using SFA.DAS.Courses.Domain.Courses;
+
+namespace SFA.DAS.Courses.Api.UnitTests.Models
+{
+    public class WhenCastingToGetStandardResponseFromDomainType
+    {
+        [Test, AutoData]
+        public void Then_Maps_Fields_Appropriately(
+            Standard source)
+        {
+            var response = (GetStandardResponse)source;
+
+            response.Should().BeEquivalentTo(source);
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Api/Controllers/StandardsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Courses.Api.ApiResponses;
+using SFA.DAS.Courses.Domain.Interfaces;
+
+namespace SFA.DAS.Courses.Api.Controllers
+{
+    [ApiController]
+    public class StandardsController : ControllerBase
+    {
+        private readonly IStandardsService _standardsService;
+
+        public StandardsController(IStandardsService standardsService)
+        {
+            _standardsService = standardsService;
+        }
+
+        public async Task<IActionResult> GetList()
+        {
+            var standards = await _standardsService.GetStandardsList();
+
+            var response = new GetStandardsListResponse
+            {
+                Standards = standards.Select(standard => (GetStandardResponse)standard)
+            };
+
+            return Ok(response);
+        }
+    }
+}
