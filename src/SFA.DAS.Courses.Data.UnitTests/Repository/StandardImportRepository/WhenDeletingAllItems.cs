86dcbc0fb7cac17cb7e1b09cb5a7e93574d951c8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-03 16:23:19 +0100

    Add Standard Import Repository
    Added standard import repository and added methods for deleting all and inserting many

--- src/SFA.DAS.Courses.Data.UnitTests/Repository/StandardImportRepository/WhenDeletingAllItems.cs
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/StandardImportRepository/WhenDeletingAllItems.cs
@@ -1,7 +1,48 @@
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Courses.Domain.Entities;
+
 namespace SFA.DAS.Courses.Data.UnitTests.Repository.StandardImportRepository
 {
     public class WhenDeletingAllItems
     {
-        
+        private Mock<ICoursesDataContext> _coursesDataContext;
+        private List<StandardImport> _standardsImport;
+        private Data.Repository.StandardImportRepository _standardImportRepository;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _standardsImport = new List<StandardImport>
+            {
+                new StandardImport()
+                {
+                    Id = 1
+                },
+                new StandardImport
+                {
+                    Id = 2
+                }
+            };
+            
+            _coursesDataContext = new Mock<ICoursesDataContext>();
+            _coursesDataContext.Setup(x => x.StandardsImport).ReturnsDbSet(_standardsImport);
+            
+
+            _standardImportRepository = new Data.Repository.StandardImportRepository(_coursesDataContext.Object);
+        }
+
+        [Test]
+        public void Then_The_StandardsImport_Items_Are_Removed()
+        {
+            //Act
+            _standardImportRepository.DeleteAll();
+            
+            //Assert
+            _coursesDataContext.Verify(x=>x.StandardsImport.RemoveRange(_coursesDataContext.Object.StandardsImport), Times.Once);
+            _coursesDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
     }
 }
--- src/SFA.DAS.Courses.Data/Repository/StandardImportRepository.cs
+++ src/SFA.DAS.Courses.Data/Repository/StandardImportRepository.cs
@@ -1,7 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.Courses.Domain.Entities;
+
 namespace SFA.DAS.Courses.Data.Repository
 {
     public class StandardImportRepository
     {
-        
+        private readonly ICoursesDataContext _coursesDataContext;
+
+        public StandardImportRepository(ICoursesDataContext coursesDataContext)
+        {
+            _coursesDataContext = coursesDataContext;
+        }
+
+        public async Task InsertMany(IEnumerable<StandardImport> standardsImport)
+        {
+            await _coursesDataContext.StandardsImport.AddRangeAsync(standardsImport);
+
+            _coursesDataContext.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            _coursesDataContext.StandardsImport.RemoveRange(_coursesDataContext.StandardsImport);
+            _coursesDataContext.SaveChanges();
+        }
     }
 }
