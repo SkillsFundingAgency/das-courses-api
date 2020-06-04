7601053eac8e0ce38f04f9c0184a5f3cc5f3f997
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-04 15:11:41 +0100

    Add command handler for import standards from API

---
+++ src/SFA.DAS.Courses.Application/StandardsImport/Handlers/ImportStandards/ImportStandardsCommand.cs
@@ -0,0 +1,9 @@
+using MediatR;
+
+namespace SFA.DAS.Courses.Application.StandardsImport.Handlers.ImportStandards
+{
+    public class ImportStandardsCommand: IRequest<Unit>
+    {
+        
+    }
+}
