619efcbd49dd552141c65c265c7c9564159d4459
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-04 09:29:41 +0100

    Add MediatR

--- src/SFA.DAS.Courses.Api/Startup.cs
+++ src/SFA.DAS.Courses.Api/Startup.cs
@@ -1,16 +1,55 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Configuration.AzureTableStorage;
 
 namespace SFA.DAS.Courses.Api
 {
     public class Startup
     {
+        private readonly IHostingEnvironment _environment;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            _environment = environment;
+            var config = new ConfigurationBuilder()
+                .AddConfiguration(configuration)
+                .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.json", true)
+                .AddJsonFile("appsettings.Development.json", true)
+#endif
+                .AddEnvironmentVariables();
+
+            if (!configuration["Environment"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+            {
+                config.AddAzureTableStorage(options =>
+                    {
+                        options.ConfigurationKeys = configuration["ConfigNames"].Split(",");
+                        options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
+                        options.EnvironmentName = configuration["Environment"];
+                        options.PreFixConfigurationKeys = false;
+                    }
+                );
+            }
+            
+            _configuration = config.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMediatR(typeof(dummy).Assembly);
+            
+            services.AddApplicationInsightsTelemetry(_configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
+            services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- src/SFA.DAS.Courses.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SFA.DAS.Courses.Application
-{
-    public class Class1
-    {
-    }
-}
