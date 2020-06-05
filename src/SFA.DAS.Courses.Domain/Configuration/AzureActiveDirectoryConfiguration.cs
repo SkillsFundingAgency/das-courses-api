8071f743a3e2ae6f7911be9cbd4629b6350554e7
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-05 09:50:16 +0100

    Add Azure AD configuration into startup

---
+++ src/SFA.DAS.Courses.Domain/Configuration/AzureActiveDirectoryConfiguration.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.Courses.Domain.Configuration
+{
+    public class AzureActiveDirectoryConfiguration
+    {
+        public string Tenant { get; set; }
+        public string Identifier { get; set; }
+        public string Id { get; set; }
+    }
+}
