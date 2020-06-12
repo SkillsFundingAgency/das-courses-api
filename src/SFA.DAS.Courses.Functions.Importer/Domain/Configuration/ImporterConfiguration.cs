0e79abc2e57081d0c90b6b1a093793b671ed3639
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-12 13:22:30 +0100

    Remove unused config vars

--- src/SFA.DAS.Courses.Functions.Importer/Domain/Configuration/ImporterConfiguration.cs
+++ src/SFA.DAS.Courses.Functions.Importer/Domain/Configuration/ImporterConfiguration.cs
@@ -3,9 +3,7 @@ namespace SFA.DAS.Courses.Functions.Importer.Domain.Configuration
     public class ImporterConfiguration
     {
         public string Url { get; set; }
-        public string Id { get; set; }
-        public string Secret { get; set; }
         public string Identifier { get; set; }
-        public string Tenant { get; set; }
+        
     }
 }
