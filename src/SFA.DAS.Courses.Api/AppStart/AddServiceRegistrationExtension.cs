9105c19c539f582bdc1a2552df4e8e70b3d6448a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-12 11:50:24 +0100

    Refactor Startup
    Added extension methods for service registration, database registration and adding authentication

---
+++ src/SFA.DAS.Courses.Api/AppStart/AddServiceRegistrationExtension.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Courses.Application.Courses.Services;
+using SFA.DAS.Courses.Application.StandardsImport.Services;
+using SFA.DAS.Courses.Data.Repository;
+using SFA.DAS.Courses.Domain.Interfaces;
+using SFA.DAS.Courses.Infrastructure.Api;
+
+namespace SFA.DAS.Courses.Api.AppStart
+{
+    public static class AddServiceRegistrationExtension
+    {
+        public static void AddServiceRegistration(this IServiceCollection services)
+        {
+            services.AddHttpClient<IInstituteOfApprenticeshipService, InstituteOfApprenticeshipService>();
+            services.AddTransient<IStandardsImportService, StandardsImportService>();
+            services.AddTransient<IStandardsService, StandardsService>();
+            services.AddTransient<IStandardImportRepository, StandardImportRepository>();
+            services.AddTransient<IStandardRepository, StandardRepository>();
+        }
+    }
+}
--- src/SFA.DAS.Courses.Api/Startup.cs
+++ src/SFA.DAS.Courses.Api/Startup.cs
@@ -1,38 +1,26 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using MediatR;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.Courses.Api.AppStart;
 using SFA.DAS.Courses.Api.Infrastructure;
-using SFA.DAS.Courses.Application.Courses.Services;
 using SFA.DAS.Courses.Application.StandardsImport.Handlers.ImportStandards;
-using SFA.DAS.Courses.Application.StandardsImport.Services;
-using SFA.DAS.Courses.Data;
-using SFA.DAS.Courses.Data.Repository;
 using SFA.DAS.Courses.Domain.Configuration;
-using SFA.DAS.Courses.Domain.Interfaces;
-using SFA.DAS.Courses.Infrastructure.Api;
-using SFA.DAS.Courses.Infrastructure.Configuration;
 
 namespace SFA.DAS.Courses.Api
 {
     public class Startup
     {
-        private readonly IHostingEnvironment _environment;
         private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        public Startup(IConfiguration configuration)
         {
-            _environment = environment;
             var config = new ConfigurationBuilder()
                 .AddConfiguration(configuration)
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -56,8 +44,7 @@ namespace SFA.DAS.Courses.Api
             
             _configuration = config.Build();
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             
@@ -68,64 +55,19 @@ namespace SFA.DAS.Courses.Api
             services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
             
             var serviceProvider = services.BuildServiceProvider();
-            var config = serviceProvider.GetService<IOptions<CoursesConfiguration>>().Value;
-            
+
             if (!ConfigurationIsLocalOrDev())
             {
-                var azureActiveDirectoryConfiguration = serviceProvider.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value;
-                services.AddAuthorization(o =>
-                {
-                    
-                    o.AddPolicy(PolicyNames.Default, policy =>
-                    {
-                        policy.RequireAuthenticatedUser();
-                        policy.RequireRole(RoleNames.ApimRequest);
-                    });
-                    o.AddPolicy(PolicyNames.HasDataLoadPolicy, policy =>
-                    {
-                        policy.RequireAuthenticatedUser();
-                        policy.RequireRole(RoleNames.DataLoad);
-                    });
-                });
-                
-                services.AddAuthentication(auth => { auth.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; })
-                .AddJwtBearer(auth =>
-                {
-                    auth.Authority =
-                        $"https://login.microsoftonline.com/{azureActiveDirectoryConfiguration.Tenant}";
-                    auth.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                    {
-                        ValidAudiences = new List<string>
-                        {
-                            azureActiveDirectoryConfiguration.Identifier,
-                            azureActiveDirectoryConfiguration.Id
-                        }
-                    };
-                });
-                services.AddSingleton<IClaimsTransformation, AzureAdScopeClaimTransformation>();
+                services.AddAuthentication(serviceProvider.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
+
             } 
-            
             
             services.AddMediatR(typeof(ImportStandardsCommand).Assembly);
 
-            services.AddHttpClient<IInstituteOfApprenticeshipService, InstituteOfApprenticeshipService>();
-            services.AddTransient<IStandardsImportService, StandardsImportService>();
-            services.AddTransient<IStandardsService, StandardsService>();
-            services.AddTransient<IStandardImportRepository, StandardImportRepository>();
-            services.AddTransient<IStandardRepository, StandardRepository>();
+            services.AddServiceRegistration();
             
-            if (_configuration["Environment"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
-            {
-                services.AddDbContext<CoursesDataContext>(options => options.UseInMemoryDatabase("SFA.DAS.Courses"));
-            }
-            else
-            {
-                services.AddDbContext<CoursesDataContext>(options => options.UseSqlServer(config.ConnectionString));
-            }
-
-            services.AddScoped<ICoursesDataContext, CoursesDataContext>(provider => provider.GetService<CoursesDataContext>());
-            services.AddTransient(provider => new Lazy<CoursesDataContext>(provider.GetService<CoursesDataContext>()));
-
+            services.AddDatabaseRegistration(serviceProvider.GetService<IOptions<CoursesConfiguration>>().Value, _configuration["Environment"]);
+            
             services
                 .AddMvc(o =>
                 {
@@ -140,7 +82,6 @@ namespace SFA.DAS.Courses.Api
             services.BuildServiceProvider();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
