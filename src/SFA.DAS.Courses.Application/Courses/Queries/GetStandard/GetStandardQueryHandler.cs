1b9be504fd96c3946d9365c4e0d03b9b79656a91
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-17 10:58:55 +0100

    Add Handler for getting standard by id

---
+++ src/SFA.DAS.Courses.Application/Courses/Queries/GetStandard/GetStandardQueryHandler.cs
@@ -0,0 +1,23 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Courses.Domain.Interfaces;
+
+namespace SFA.DAS.Courses.Application.Courses.Queries.GetStandard
+{
+    public class GetStandardQueryHandler : IRequestHandler<GetStandardQuery, GetStandardResult>
+    {
+        private readonly IStandardsService _standardsService;
+
+        public GetStandardQueryHandler (IStandardsService standardsService)
+        {
+            _standardsService = standardsService;
+        }
+        public async Task<GetStandardResult> Handle(GetStandardQuery request, CancellationToken cancellationToken)
+        {
+            var standard = await _standardsService.GetStandard(request.StandardId);
+            
+            return new GetStandardResult { Standard = standard };
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Application/Courses/Queries/GetStandard/GetStandardResult.cs
@@ -0,0 +1,9 @@
+using SFA.DAS.Courses.Domain.Courses;
+
+namespace SFA.DAS.Courses.Application.Courses.Queries.GetStandard
+{
+    public class GetStandardResult
+    {
+        public Standard Standard { get ; set ; }
+    }
+}
