0d1db2596717ba62bf7d109984b9f30418c852d7
Scott Wakefield <scott@expertcc.co.uk>
2020-06-23 08:46:39 +0100

    FAT2-87 adding tests for ordering and scoring.

--- src/SFA.DAS.Courses.Data.UnitTests/Search/WhenSearchingACourseIndex.cs
+++ src/SFA.DAS.Courses.Data.UnitTests/Search/WhenSearchingACourseIndex.cs
@@ -22,8 +22,8 @@ namespace SFA.DAS.Courses.Data.UnitTests.Search
         private readonly List<Standard> _standards= new List<Standard>
         {
             new Standard{Id = DentalTechId, Title = "Dental technician (integrated)", TypicalJobTitles = "denture maker", Keywords = "dentistry|removable prosthesis|impression trays|orthodontics|dentures|teeth"},
-            new Standard{Id = DentalLabAsstId, Title = "Dental laboratory assistant", TypicalJobTitles = "denture maker", Keywords = "dentistry|dental devices|orthodontics|crowns|bridges|dentures|teeth"},
-            new Standard{Id = DentalPracticeMgrId, Title = "Dental practice manager", TypicalJobTitles = "Denture Maker", Keywords = "Denture Maker"},
+            new Standard{Id = DentalLabAsstId, Title = "Laboratory assistant", TypicalJobTitles = "denture maker", Keywords = "dentistry|dental devices|orthodontics|crowns|bridges|dentures|teeth"},
+            new Standard{Id = DentalPracticeMgrId, Title = "Practice manager", TypicalJobTitles = "Denture Maker|Dental practice", Keywords = "Denture Maker"},
             new Standard{Id = OutdoorId, Title = "Outdoor activity instructor", TypicalJobTitles = "", Keywords = "Outdoor activity instructor|canoeing|sailing|climbing|surfing|cycling|hillwalking|archery|bushcraft|rock poolings|geology|plant identification|habitat|wildlife walk"},
             new Standard{Id = NetworkId, Title = "Network engineer", TypicalJobTitles = "Network Technician|Network Engineer", Keywords = "communication|networks"},
             new Standard{Id = DeveloperId, Title = "Software developer", TypicalJobTitles = "Web Developer|Application Developer", Keywords = "coding|technology"}
@@ -44,70 +44,51 @@ namespace SFA.DAS.Courses.Data.UnitTests.Search
             _searchManager = new CoursesSearchManager(directoryFactory);
         }
 
-        [Test]
-        public void Then_Searches_By_Title_Case_Insensitive()
+        [TestCase("software", 1, DeveloperId, TestName = "Title case insensitive")]
+        [TestCase("softawre", 1, DeveloperId, TestName = "Title soundex")]
+        [TestCase("application", 1, DeveloperId, TestName = "TypicalJobTitles case insensitive")]
+        [TestCase("applacation", 1, DeveloperId, TestName = "TypicalJobTitles soundex")]
+        [TestCase("coding", 1, DeveloperId, TestName = "Keywords case insensitive")]
+        [TestCase("kodng", 1, DeveloperId, TestName = "Keywords soundex")]
+        public void Then_Searches_Test_Cases(string searchTerm, int expectedCount, int expectedId)
         {
-            var searchTerm = "software";
+            var result = _searchManager.Query(searchTerm);
+
+            result.Standards.Count().Should().Be(expectedCount);
+            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == expectedId);
+        }
+        
+        [Test]
+        public void Then_Scores_By_Title_Then_TypicalJobTitles_Then_Keywords()
+        {
+            var searchTerm = "dental";
             
             var result = _searchManager.Query(searchTerm);
 
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
+            result.Standards.Count().Should().Be(3);
+            var dentalTech = result.Standards.Single(searchResult => searchResult.Id == DentalTechId);
+            var dentalLabAsst = result.Standards.Single(searchResult => searchResult.Id == DentalLabAsstId);
+            var dentalPracticeMgr = result.Standards.Single(searchResult => searchResult.Id == DentalPracticeMgrId);
+
+            dentalTech.Score.Should().BeGreaterThan(dentalPracticeMgr.Score);
+            dentalPracticeMgr.Score.Should().BeGreaterThan(dentalLabAsst.Score);
         }
 
         [Test]
-        public void Then_Searches_By_Title_Soundex()
+        public void Then_Orders_By_Score()
         {
-            var searchTerm = "softawre";
+            var searchTerm = "dental";
             
             var result = _searchManager.Query(searchTerm);
 
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
-        }
-
-        [Test]
-        public void Then_Searches_By_TypicalJobTitles_Case_Insensitive()
-        {
-            var searchTerm = "application";
-            
-            var result = _searchManager.Query(searchTerm);
-
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
-        }
-
-        [Test]
-        public void Then_Searches_By_TypicalJobTitles_Soundex()
-        {
-            var searchTerm = "applacation";
-            
-            var result = _searchManager.Query(searchTerm);
-
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
-        }
-
-        [Test]
-        public void Then_Searches_By_Keywords_Case_Insensitive()
-        {
-            var searchTerm = "coding";
-            
-            var result = _searchManager.Query(searchTerm);
-
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
-        }
-
-        [Test]
-        public void Then_Searches_By_Keywords_Soundex()
-        {
-            var searchTerm = "kodng";
-            
-            var result = _searchManager.Query(searchTerm);
-
-            result.Standards.Count().Should().Be(1);
-            result.Standards.ToList().Should().Contain(searchResult => searchResult.Id == DeveloperId);
+            result.Standards.Count().Should().Be(3);
+            result.Standards.Select((searchResult, position) => new {searchResult.Id, position}).ToList()
+                .Should().BeEquivalentTo(new List<dynamic>
+                {
+                    new {Id = DentalTechId, position = 0},
+                    new {Id = DentalPracticeMgrId, position = 1},
+                    new {Id = DentalLabAsstId, position = 2},
+                });
         }
     }
 }
