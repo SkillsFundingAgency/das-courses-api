e3b1ec6da10bd517bae26bd6008c2261d904acc8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-29 15:20:25 +0100

    Constants for LARS location

--- src/SFA.DAS.Courses.Domain/Configuration/Constants.cs
+++ src/SFA.DAS.Courses.Domain/Configuration/Constants.cs
@@ -3,5 +3,7 @@ namespace SFA.DAS.Courses.Domain.Configuration
     public static class Constants
     {
         public static string InstituteOfApprenticeshipsStandardsUrl => "https://www.instituteforapprenticeships.org/api/apprenticeshipstandards/";
+        public static string LarsBasePageUrl => "https://hub.fasst.org.uk";
+        public static string LarsDownloadPageUrl => $"{LarsBasePageUrl}/Learning%20Aims/Downloads/Pages/default.aspx";
     }
 }
