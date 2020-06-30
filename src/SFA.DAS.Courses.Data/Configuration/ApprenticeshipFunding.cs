3fd5f7eaee412844daa75886cd6196b1899449c1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-30 21:17:15 +0100

    Entity mapping for LarsStandard and ApprenticeshipFunding

---
+++ src/SFA.DAS.Courses.Data/Configuration/ApprenticeshipFunding.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SFA.DAS.Courses.Data.Configuration
+{
+    public class ApprenticeshipFunding : IEntityTypeConfiguration<Domain.Entities.ApprenticeshipFunding>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.ApprenticeshipFunding> builder)
+        {
+            builder.ToTable("ApprenticeshipFunding");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("uniqueidentifier").IsRequired();
+            builder.Property(x => x.StandardId).HasColumnName("StandardId").HasColumnType("int").IsRequired();
+            builder.Property(x => x.EffectiveFrom).HasColumnName("EffectiveFrom").HasColumnType("datetime").IsRequired();
+            builder.Property(x => x.EffectiveTo).HasColumnName("EffectiveTo").HasColumnType("datetime").IsRequired(false);
+            builder.Property(x => x.MaxEmployerLevyCap).HasColumnName("MaxEmployerLevyCap").HasColumnType("int").IsRequired();
+
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Data/Configuration/LarsStandard.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SFA.DAS.Courses.Data.Configuration
+{
+    public class LarsStandard : IEntityTypeConfiguration<Domain.Entities.LarsStandard>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.LarsStandard> builder)
+        {
+            builder.ToTable("LarsStandard");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("uniqueidentifier").IsRequired();
+            builder.Property(x => x.StandardId).HasColumnName("StandardId").HasColumnType("int").IsRequired();
+            builder.Property(x => x.Version).HasColumnName("Version").HasColumnType("int").IsRequired();
+            builder.Property(x => x.EffectiveFrom).HasColumnName("EffectiveFrom").HasColumnType("datetime").IsRequired();
+            builder.Property(x => x.EffectiveTo).HasColumnName("EffectiveTo").HasColumnType("datetime").IsRequired(false);
+            builder.Property(x => x.LastDateStarts).HasColumnName("LastDateStarts").HasColumnType("datetime").IsRequired(false);
+        }
+    }
+}
