693bb1416521b06b1a153bf8b305af5b492c4578
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-07 17:10:24 +0100

    Built & set-up LarsHealthCheckClass

---
+++ src/SFA.DAS.Courses.Infrastructure/HealthCheck/LarsHealthCheck.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SFA.DAS.Courses.Data;
+using SFA.DAS.Courses.Data.Repository;
+using SFA.DAS.Courses.Domain.Entities;
+using SFA.DAS.Courses.Domain.Interfaces;
+
+namespace SFA.DAS.Courses.Infrastructure.HealthCheck
+{
+    public class LarsHealthCheck : IHealthCheck
+    {
+        private const string HealthCheckResultDescription = "LARS Input Health Check";
+        private IImportAuditRepository _importData;
+
+
+        public LarsHealthCheck(IImportAuditRepository importData)
+        {
+            _importData = importData;
+        }
+        
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var timer = Stopwatch.StartNew();
+            var latestLarsData = _importData.GetLastImportByType(ImportType.LarsImport);
+            var latestIfateData = _importData.GetLastImportByType(ImportType.IFATEImport);
+
+            var dateNow = DateTime.UtcNow;
+            // When did LARS data import start?
+            var larsDataImportTimeStart = latestLarsData.Result.TimeStarted;
+            // When did LARS data import start?
+            var iFateDataImportTimeStart = latestIfateData.Result.TimeStarted;
+            var overTwentyFiveHours = dateNow.AddHours(-25);
+            var overTwoWeeks = dateNow.AddDays(-14);
+
+            // Compare start time to now
+            var timeResultLars = DateTime.Compare(larsDataImportTimeStart, overTwentyFiveHours);
+            var timeResultIfate = DateTime.Compare(iFateDataImportTimeStart, overTwentyFiveHours);
+
+            var timeResultIfateTwoWeeks = DateTime.Compare(iFateDataImportTimeStart, overTwoWeeks);
+            var timeResultLarsTwoWeeks = DateTime.Compare(iFateDataImportTimeStart, overTwoWeeks);
+
+            // AC1 If course data load is over 25 hours old or rows imported is zero then the health is shown as degraded.
+            if (timeResultIfate < 0 || latestIfateData.Result.RowsImported == 0)
+            {
+                // show as degraded
+            }
+
+            // AC2 If LARS data load is over 25 hours old then the health is shown as degraded
+            if (timeResultLars < 0)
+            {
+                // show degraded
+            }
+
+            // AC3 If course and LARS data is over 2 weeks and an hour old or rows imported is zero then the health is shown as degraded
+            if (timeResultIfateTwoWeeks < 0 || timeResultLarsTwoWeeks < 0 || latestIfateData.Result.RowsImported == 0 || latestLarsData.Result.RowsImported == 0)
+            {
+                // show as degraded
+            }
+        }
+    }
+}
