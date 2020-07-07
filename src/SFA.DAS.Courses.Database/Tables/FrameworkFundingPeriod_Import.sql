d054441cf4811296df7143e9dac489399a3836ea
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-07 17:19:49 +0100

    Add framework DB tables and import tables

---
+++ src/SFA.DAS.Courses.Database/Tables/FrameworkFundingPeriod_Import.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FrameworkFundingPeriod_Import]
+(
+    [Id] INT PRIMARY KEY,
+	[FrameworkId] VARCHAR(15) NOT NULL,
+	[EffectiveFrom] DATETIME NOT NULL,
+	[EffectiveTo] DATETIME NULL,
+	[FundingCap] INT NOT NULL
+)
+GO
