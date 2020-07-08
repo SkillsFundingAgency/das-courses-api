5a75b46cfa81c5f2b5fafdc65b5379fb644e8471
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-08 15:25:04 +0100

    Refactor code: remove needless DateTime ste-up and add 'await'

--- src/SFA.DAS.Courses.Infrastructure/HealthCheck/iFateHealthCheck.cs
+++ src/SFA.DAS.Courses.Infrastructure/HealthCheck/iFateHealthCheck.cs
@@ -25,37 +25,18 @@ namespace SFA.DAS.Courses.Infrastructure.HealthCheck
             var timer = Stopwatch.StartNew();
 
             var healthStatusDegraded = HealthStatus.Degraded;
-            var latestIfateData = _importData.GetLastImportByType(ImportType.IFATEImport);
-            var iFateDataImportTimeStart = latestIfateData.Result.TimeStarted;
-
-            // AC timeframes
-            var dateNow = DateTime.UtcNow;
-            var overTwentyFiveHours = dateNow.AddHours(-25);
-            var twoWeeks = dateNow.AddDays(-14);
-
-            // Is start time over 25 hours?
-            var timeResultIfate = DateTime.Compare(iFateDataImportTimeStart, overTwentyFiveHours);
-            // Compare start time to two weeks and an hour ago
-            var timeResultIfateTwoWeeksAndAnHour = DateTime.Compare(iFateDataImportTimeStart, twoWeeks.AddHours(-1));
+            var latestIfateData = await _importData.GetLastImportByType(ImportType.IFATEImport);
 
             timer.Stop();
             var durationString = timer.Elapsed.ToHumanReadableString();
 
-            // AC1 If course data load is over 25 hours old or rows imported is zero then the health is shown as degraded.
-            // AC3 If course data is over 2 weeks and an hour old or rows imported is zero then the health is shown as degraded
-            if (timeResultIfate > 0 || latestIfateData.Result.RowsImported == 0 || timeResultIfateTwoWeeksAndAnHour > 0)
+            // AC1 If IFATE data load is over 25 hours old or rows imported is zero then the health is shown as degraded
+            if (DateTime.UtcNow >= latestIfateData.TimeStarted.AddHours(25) || latestIfateData.RowsImported == 0)
             {
                 return new HealthCheckResult(healthStatusDegraded, "Course data load is over 25 hours old or rows imported are zero", null, new Dictionary<string, object> { { "Dictionary", durationString } });
             }
 
-            // AC4 If the courses data is under 2 weeks and an hour old then the site is shown as healthy
-            if (timeResultIfateTwoWeeksAndAnHour < 0)
-            {
-                return HealthCheckResult.Healthy("IFATE and LARS data are both under two weeks (and an hour) old", new Dictionary<string, object> { { "Duration", durationString } });
-            }
-
             return HealthCheckResult.Healthy(HealthCheckResultDescription, new Dictionary<string, object> { { "Duration", durationString } });
-
         }
     }
 }
