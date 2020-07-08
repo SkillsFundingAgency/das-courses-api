04f70975b931cb4a005ba8739ebfbbe8e71c7bd6
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-08 06:15:08 +0100

    Create entity configuration for Framework types

---
+++ src/SFA.DAS.Courses.Data/Configuration/FrameworkFunding.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SFA.DAS.Courses.Data.Configuration
+{
+    public class FrameworkFunding : IEntityTypeConfiguration<Domain.Entities.FrameworkFunding>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.FrameworkFunding> builder)
+        {
+            builder.ToTable("FrameworkFundingPeriod");
+            builder.HasKey(x=>x.Id);
+
+            builder.Property(x => x.Id).HasColumnName("Id").HasColumnType("int").IsRequired().ValueGeneratedOnAdd();
+            builder.Property(x => x.FrameworkId).HasColumnName("FrameworkId").HasColumnType("varchar").HasMaxLength(15).IsRequired();
+            builder.Property(x => x.EffectiveFrom).HasColumnName("EffectiveFrom").HasColumnType("datetime").IsRequired();
+            builder.Property(x => x.EffectiveTo).HasColumnName("EffectiveTo").HasColumnType("datetime").IsRequired(false);
+            builder.Property(x => x.FundingCap).HasColumnName("FundingCap").HasColumnType("int").IsRequired();
+
+            builder.HasOne(c => c.Framework)
+                .WithMany(c => c.FundingPeriods)
+                .HasForeignKey(c => c.FrameworkId)
+                .HasPrincipalKey(c => c.Id);
+            
+            builder.HasIndex(x => x.Id).IsUnique();
+        }
+    }
+}
