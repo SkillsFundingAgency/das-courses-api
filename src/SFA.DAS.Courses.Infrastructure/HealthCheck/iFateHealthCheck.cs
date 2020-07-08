fa709d3042ee9be3c9e321f9c86e095f1e73592c
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-07-08 12:13:04 +0100

    Remove needless code

--- src/SFA.DAS.Courses.Infrastructure/HealthCheck/iFateHealthCheck.cs
+++ src/SFA.DAS.Courses.Infrastructure/HealthCheck/iFateHealthCheck.cs
@@ -23,50 +23,34 @@ namespace SFA.DAS.Courses.Infrastructure.HealthCheck
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             var timer = Stopwatch.StartNew();
+
             var healthStatusDegraded = HealthStatus.Degraded;
             var latestIfateData = _importData.GetLastImportByType(ImportType.IFATEImport);
-
-            // What time is it now?
-            var dateNow = DateTime.UtcNow;
-
-            // When did IFATE data import start?
             var iFateDataImportTimeStart = latestIfateData.Result.TimeStarted;
 
-            // Time stamp: over 25 hours ago
+            // AC timeframes
+            var dateNow = DateTime.UtcNow;
             var overTwentyFiveHours = dateNow.AddHours(-25);
-            // Time stamp: two weeks ago
-            var overTwoWeeks = dateNow.AddDays(-14);
+            var twoWeeks = dateNow.AddDays(-14);
 
-            // Compare start time to now
+            // Is start time over 25 hours?
             var timeResultIfate = DateTime.Compare(iFateDataImportTimeStart, overTwentyFiveHours);
-
-            // Compare start time to two weeks ago
-            var timeResultIfateTwoWeeks = DateTime.Compare(iFateDataImportTimeStart, overTwoWeeks);
-
-            // Compare start time to two weeks ago (minus an hour)
-            var timeResultIfateTwoWeeksMinusOneHour = DateTime.Compare(iFateDataImportTimeStart, overTwoWeeks);
+            // Compare start time to two weeks and an hour ago
+            var timeResultIfateTwoWeeksAndAnHour = DateTime.Compare(iFateDataImportTimeStart, twoWeeks.AddHours(-1));
 
             timer.Stop();
             var durationString = timer.Elapsed.ToHumanReadableString();
 
             // AC1 If course data load is over 25 hours old or rows imported is zero then the health is shown as degraded.
-            if (timeResultIfate > 0 || latestIfateData.Result.RowsImported == 0)
+            // AC3 If course data is over 2 weeks and an hour old or rows imported is zero then the health is shown as degraded
+            if (timeResultIfate > 0 || latestIfateData.Result.RowsImported == 0 || timeResultIfateTwoWeeksAndAnHour > 0)
             {
-                // show as degraded
                 return new HealthCheckResult(healthStatusDegraded, "Course data load is over 25 hours old or rows imported are zero", null, new Dictionary<string, object> { { "Dictionary", durationString } });
             }
 
-            // AC3 If course data is over 2 weeks and an hour old or rows imported is zero then the health is shown as degraded
-            if (timeResultIfateTwoWeeks > 0 || latestIfateData.Result.RowsImported == 0)
-            {
-                // show as degraded
-                return new HealthCheckResult(healthStatusDegraded, "IFATE data is over two weeks (and an hour) old or rows imported are zero", null, new Dictionary<string, object> { { "Dictionary", durationString } });
-            }
-
             // AC4 If the courses data is under 2 weeks and an hour old then the site is shown as healthy
-            if (timeResultIfateTwoWeeksMinusOneHour < 0)
+            if (timeResultIfateTwoWeeksAndAnHour < 0)
             {
-                // show as healthy
                 return HealthCheckResult.Healthy("IFATE and LARS data are both under two weeks (and an hour) old", new Dictionary<string, object> { { "Duration", durationString } });
             }
 
