6b54edabb53605c1d1c23941a51f44be029ff872
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-08 10:52:57 +0100

    Add framework import repository and tests

---
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/FrameworkImportRepository/WhenGettingAllItems.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.UnitTests.Repository.FrameworkImportRepository
+{
+    public class WhenGettingAllItems
+    {
+        private List<FrameworkImport> _frameworkImports;  
+        private Mock<ICoursesDataContext> _coursesDataContext;
+        private Data.Repository.FrameworkImportRepository _frameworkImportRepository;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _frameworkImports = new List<FrameworkImport>
+            {
+                new FrameworkImport
+                {
+                    Id = "1-1-1"
+                },
+                new FrameworkImport
+                {
+                    Id = "2-2-2"
+                }
+            };
+            
+            _coursesDataContext = new Mock<ICoursesDataContext>();
+            _coursesDataContext.Setup(x => x.FrameworksImport).ReturnsDbSet(_frameworkImports);
+            
+            _frameworkImportRepository = new Data.Repository.FrameworkImportRepository(_coursesDataContext.Object);
+        }
+
+        [Test]
+        public async Task Then_The_FrameworkImport_Items_Are_Returned()
+        {
+            //Act
+            var sectorsImport = await _frameworkImportRepository.GetAll();
+            
+            //Assert
+            Assert.IsNotNull(sectorsImport);
+            sectorsImport.Should().BeEquivalentTo(_frameworkImports);
+        }
+    }
+}
