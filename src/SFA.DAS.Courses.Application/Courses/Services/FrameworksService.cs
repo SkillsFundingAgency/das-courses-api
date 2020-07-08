481766aad9f32d55304312de71030f64b77d0b1a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-08 18:58:54 +0100

    Add frameworks service
    Add methods for get by id and get all frameworks

---
+++ src/SFA.DAS.Courses.Application/Courses/Services/FrameworksService.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SFA.DAS.Courses.Domain.Courses;
+using SFA.DAS.Courses.Domain.Interfaces;
+
+namespace SFA.DAS.Courses.Application.Courses.Services
+{
+    public class FrameworksService
+    {
+        private readonly IFrameworkRepository _frameworkRepository;
+
+        public FrameworksService (IFrameworkRepository frameworkRepository)
+        {
+            _frameworkRepository = frameworkRepository;
+        }
+        public async Task<Framework> GetFramework(string frameworkId)
+        {
+            var framework = await _frameworkRepository.Get(frameworkId);
+
+            return framework;
+        }
+
+        public async Task<IEnumerable<Framework>> GetFrameworks()
+        {
+            var frameworks = await _frameworkRepository.GetAll();
+
+            return frameworks.Select(c => (Framework) c).ToList();
+        }
+    }
+}
