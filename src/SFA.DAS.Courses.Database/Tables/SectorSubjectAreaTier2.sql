db5771b665b5b346030c2d7caac06df8858525dd
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-05 12:48:45 +0100

    Add effectiveto to SectorSubjectAreaTier2 tables

--- src/SFA.DAS.Courses.Database/Tables/SectorSubjectAreaTier2.sql
+++ src/SFA.DAS.Courses.Database/Tables/SectorSubjectAreaTier2.sql
@@ -2,7 +2,8 @@
 (
 	[SectorSubjectAreaTier2] decimal NOT NULL PRIMARY KEY,
 	[SectorSubjectAreaTier2Desc] VARCHAR(500) NOT NULL,
-	[EffectiveFrom] DATETIME NOT NULL
+	[EffectiveFrom] DATETIME NOT NULL,
+	[EffectiveTo] DATETIME NULL
 )
 GO
 
