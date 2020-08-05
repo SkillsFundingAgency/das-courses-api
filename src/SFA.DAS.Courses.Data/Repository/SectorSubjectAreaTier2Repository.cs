135005518e77d0975305173ab48583e1828b6f09
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-05 13:34:41 +0100

    Add SubjectAreaTier2Repository
    Added repository with insert many and delete many

---
+++ src/SFA.DAS.Courses.Data/Repository/SectorSubjectAreaTier2Repository.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.Repository
+{
+    public class SectorSubjectAreaTier2Repository
+    {
+        private readonly ICoursesDataContext _coursesDataContext;
+
+        public SectorSubjectAreaTier2Repository (ICoursesDataContext coursesDataContext)
+        {
+            _coursesDataContext = coursesDataContext;
+        }
+
+        public void DeleteAll()
+        {
+            _coursesDataContext.SectorSubjectAreaTier2.RemoveRange(_coursesDataContext.SectorSubjectAreaTier2);
+            _coursesDataContext.SaveChanges();
+        }
+
+        public async Task InsertMany(IEnumerable<SectorSubjectAreaTier2> sectorSubjectAreaTier2Items)
+        {
+            await _coursesDataContext.SectorSubjectAreaTier2.AddRangeAsync(sectorSubjectAreaTier2Items);
+            _coursesDataContext.SaveChanges();
+        }
+    }
+}
