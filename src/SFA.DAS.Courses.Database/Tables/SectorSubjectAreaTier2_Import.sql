376815a33a31c77ce681b53429f42beb411c218c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-05 12:45:14 +0100

    Add SectorSubjectAreaTier2 tables

---
+++ src/SFA.DAS.Courses.Database/Tables/SectorSubjectAreaTier2_Import.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SectorSubjectAreaTier2_Import]
+(
+	[SectorSubjectAreaTier2] decimal NOT NULL PRIMARY KEY,
+	[SectorSubjectAreaTier2Desc] VARCHAR(500) NOT NULL,
+	[EffectiveFrom] DATETIME NOT NULL
+)
+GO
+
+ 
