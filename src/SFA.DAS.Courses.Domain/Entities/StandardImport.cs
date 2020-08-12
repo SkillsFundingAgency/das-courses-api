fa2db5ba5f8f736a5e3f80ba683a6e788e95f5b7
robyoung91 <robertmatthewyoung@gmail.com>
2020-08-12 14:45:51 +0100

    Amend coreSkillsCount assignment in implicit operator
    * Amend assignment to check for CoreAndOptions - will return all skills
    if false, only skills mapped to a core duty if true

--- src/SFA.DAS.Courses.Domain/Entities/StandardImport.cs
+++ src/SFA.DAS.Courses.Domain/Entities/StandardImport.cs
@@ -1,4 +1,6 @@
+﻿using System;
 using System.Linq;
+using SFA.DAS.Courses.Domain.ImportTypes;
 
 namespace SFA.DAS.Courses.Domain.Entities
 {
@@ -6,9 +8,25 @@ namespace SFA.DAS.Courses.Domain.Entities
     {
         public static implicit operator StandardImport(Domain.ImportTypes.Standard standard)
         {
-            //TODO
-            var coreSkillsCount = standard.Duties.Any() ? string.Join("|", standard.Duties.Where(c=>c.IsThisACoreDuty.Equals(1)).Select(c=>c.DutyDetail)) : null;
+            string coreSkillsCount = null;
+
+            //Check duties and skills not empty
+            if (standard.Duties.Any() && standard.Skills.Any())
+            {
+                if(standard.CoreAndOptions)
+                {   
+                    //Put all of the MappedSkills skillIds from all of the core duties into a list
+                    var mappedSkillsList = standard.Duties.Where(d => d.IsThisACoreDuty.Equals(1)).SelectMany(d => d.MappedSkills).ToList();
+                    //Store the detail property for each skill in standard.Skills who's skillId appears in the list of mapped skills for core duties
+                    coreSkillsCount = string.Join("|", standard.Skills.Where(s => mappedSkillsList.Contains(s.SkillId)).Select(s => s.Detail));
+                } else
+                {   
+                    //Stores all skill details from skills in standard.Skills if CoreAndOptions false
+                    coreSkillsCount = string.Join("|", standard.Skills.Select(s => s.Detail));
+                }
+            }
             
+
             return new StandardImport
             {
                 Id = standard.LarsCode,
@@ -21,7 +39,7 @@ namespace SFA.DAS.Courses.Domain.Entities
                 TypicalDuration = standard.TypicalDuration,
                 TypicalJobTitles = string.Join("|", standard.TypicalJobTitles),
                 Version = standard.Version,
-                Keywords = standard.Keywords.Any() ? string.Join("|",standard.Keywords) : null,
+                Keywords = standard.Keywords.Any() ? string.Join("|", standard.Keywords) : null,
                 RouteId = standard.RouteId
             };
         }
