730056cf998d2132b7b1f9c83a62fea57cf1a523
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-10-07 19:23:24 +0100

    Content change:  rename course levels

--- src/SFA.DAS.Courses.Domain/Courses/LevelsConstant.cs
+++ src/SFA.DAS.Courses.Domain/Courses/LevelsConstant.cs
@@ -6,10 +6,10 @@ namespace SFA.DAS.Courses.Domain.Courses
     {
         public static readonly IEnumerable<Level> Levels = new List<Level>
         {
-            new Level {Code = 2, Name = "GCSE grades 9 to 4"},
+            new Level {Code = 2, Name = "GCSE"},
             new Level {Code = 3, Name = "A level"},
-            new Level {Code = 4, Name = "HNC"},
-            new Level {Code = 5, Name = "foundation degree"},
+            new Level {Code = 4, Name = "higher national certificate (HNC)"},
+            new Level {Code = 5, Name = "higher national diploma (HND)"},
             new Level {Code = 6, Name = "degree"},
             new Level {Code = 7, Name = "master’s degree"}
         };
