f5c948c0231398442a5baaed75d8f120c3cd3628
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-12 16:26:46 +0000

    FAT2-252 - Format change for logger

--- src/SFA.DAS.Courses.Application/Courses/Queries/GetStandardsList/GetStandardsListQueryHandler.cs
+++ src/SFA.DAS.Courses.Application/Courses/Queries/GetStandardsList/GetStandardsListQueryHandler.cs
@@ -27,7 +27,7 @@ namespace SFA.DAS.Courses.Application.Courses.Queries.GetStandardsList
 
             if (standards.Count == 0 && !string.IsNullOrWhiteSpace(request.Keyword))
             {
-                _logger.LogInformation($"Zero results for searching by keyword [{request.Keyword}]");
+                _logger.LogInformation($"Zero results for searching by keyword [{request.Keyword}]", new {request.Keyword});
             }
 
             return new GetStandardsListResult
