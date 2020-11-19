55c228cb16fee2e324bd264a22557ca76fb3b0cc
dev4valtech <dev4valtech@gmail.com>
2020-11-19 09:19:30 +0000

    Minor fixes

--- src/SFA.DAS.Courses.Application/CoursesImport/Handlers/ImportStandards/ImportStandardsCommandHandler.cs
+++ src/SFA.DAS.Courses.Application/CoursesImport/Handlers/ImportStandards/ImportStandardsCommandHandler.cs
@@ -31,23 +31,4 @@ namespace SFA.DAS.Courses.Application.CoursesImport.Handlers.ImportStandards
             return Unit.Value;
         }
     }
-
-    public class ImportStandardDocumentsCommandHandler : IRequestHandler<ImportStandardDocumentsCommand, Unit>
-    {
-        private readonly IInstituteOfApprenticeshipService instituteOfApprenticeshipService;
-        private readonly IStandardDocumentRepository standardDocumentRepository;
-
-        public ImportStandardDocumentsCommandHandler(IInstituteOfApprenticeshipService instituteOfApprenticeshipService, IStandardDocumentRepository standardDocumentRepository)
-        {
-            this.instituteOfApprenticeshipService = instituteOfApprenticeshipService;
-            this.standardDocumentRepository = standardDocumentRepository;
-        }
-
-        public async Task<Unit> Handle(ImportStandardDocumentsCommand request, CancellationToken cancellationToken)
-        {
-            var importedStandards = await instituteOfApprenticeshipService.GetStandardDocuments();
-            var keys = importedStandards.Select(x => standardDocumentRepository.SaveDocument(x.Key, x.Value)).ToList();
-            return Unit.Value;
-        }
-    }
 }
--- src/SFA.DAS.Courses.Data/Repository/StandardDocumentRepository.cs
+++ src/SFA.DAS.Courses.Data/Repository/StandardDocumentRepository.cs
@@ -14,25 +14,17 @@ namespace SFA.DAS.Courses.Data.Repository
         public StandardDocumentRepository()
         {
             var client = new MongoClient("mongodb://localhost:C2y6yDjf5%2FR%2Bob0N8A7Cgv30VRDJIWEHLM%2B4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw%2FJw%3D%3D@localhost:10255/admin?ssl=true");
-            var database = client.GetDatabase("Standards");
-            standardCollection = database.GetCollection<BsonDocument>("Standards");
+            var database = client.GetDatabase("standards");
+            standardCollection = database.GetCollection<BsonDocument>("standards");
             retryPolicy = Policy.Handle<MongoException>().WaitAndRetry(new[] { TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2), TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(4) });
         }
 
         public string SaveDocument(string key, string content)
         {
-            try
-            {
-                var document = BsonDocument.Parse(content);
-                document.Add(new BsonElement("_id", key));
-                //await retryPolicy.ExecuteAsync(_ => standardCollection.ReplaceOneAsync(new BsonDocument("_id", key), document), new Context(nameof(SaveDocument)));
-                retryPolicy.Execute(_ => standardCollection.InsertOne(document), new Context(nameof(SaveDocument)));
-            }
-            catch (Exception ex)
-            {
-                var a = 1;
-                var b = a + 1;
-            }
+            var document = BsonDocument.Parse(content);
+            document.Add(new BsonElement("_id", key));
+            //await retryPolicy.ExecuteAsync(_ => standardCollection.ReplaceOneAsync(new BsonDocument("_id", key), document), new Context(nameof(SaveDocument)));
+            retryPolicy.Execute(_ => standardCollection.InsertOne(document), new Context(nameof(SaveDocument)));
             return key;
         }
     }
