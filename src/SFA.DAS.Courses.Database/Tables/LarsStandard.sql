5c4ffaa261ffe93738b066f8ef0414790f342b45
dev4valtech <dev4valtech@gmail.com>
2021-01-27 15:47:17 +0000

    :boom: Updated table definitions to incorporate versioning change

--- src/SFA.DAS.Courses.Database/Tables/LarsStandard.sql
+++ src/SFA.DAS.Courses.Database/Tables/LarsStandard.sql
@@ -1,7 +1,6 @@
 ﻿CREATE TABLE [dbo].[LarsStandard]
 (
-	[Id] UNIQUEIDENTIFIER PRIMARY KEY,
-	[StandardId] INT NOT NULL,
+	[LarsCode] INT PRIMARY KEY,
 	[Version] INT NOT NULL,
 	[EffectiveFrom] DATETIME NOT NULL,
 	[EffectiveTo] DATETIME NULL,
@@ -11,6 +10,3 @@
 )
 GO
 
-CREATE NONCLUSTERED INDEX [IDX_LarsStandard_StandardId] ON [dbo].[LarsStandard] (StandardId) 
-INCLUDE (Id,Version,EffectiveFrom,EffectiveTo,  LastDateStarts, SectorSubjectAreaTier2) WITH (ONLINE = ON) 
-GO 
