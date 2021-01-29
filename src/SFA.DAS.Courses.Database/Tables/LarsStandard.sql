831faf9191c1b4bcf70923c10191a47a764d99aa
dev4valtech <dev4valtech@gmail.com>
2021-01-29 10:28:06 +0000

    Revert ":boom: Updated table definitions to incorporate versioning change"
    This reverts commit 4c05d8e2e438394a84374b1a9d2cdb6fb3ec9641.

--- src/SFA.DAS.Courses.Database/Tables/LarsStandard.sql
+++ src/SFA.DAS.Courses.Database/Tables/LarsStandard.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[LarsStandard]
 (
-	[LarsCode] INT PRIMARY KEY,
+	[Id] UNIQUEIDENTIFIER PRIMARY KEY,
+	[StandardId] INT NOT NULL,
 	[Version] INT NOT NULL,
 	[EffectiveFrom] DATETIME NOT NULL,
 	[EffectiveTo] DATETIME NULL,
@@ -10,3 +11,6 @@
 )
 GO
 
+CREATE NONCLUSTERED INDEX [IDX_LarsStandard_StandardId] ON [dbo].[LarsStandard] (StandardId) 
+INCLUDE (Id,Version,EffectiveFrom,EffectiveTo,  LastDateStarts, SectorSubjectAreaTier2) WITH (ONLINE = ON) 
+GO 
