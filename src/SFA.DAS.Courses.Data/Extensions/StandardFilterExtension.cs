c4ca9b3f4449741799c28a3df7d7e42fa9f3da4f
codewolf86 <chris@codewolfconsulting.com>
2021-01-29 11:55:46 +0000

    Initial Addition of filters

--- src/SFA.DAS.Courses.Data/Extensions/StandardFilterExtension.cs
+++ src/SFA.DAS.Courses.Data/Extensions/StandardFilterExtension.cs
@@ -17,6 +17,9 @@ namespace SFA.DAS.Courses.Data.Extensions
                 case StandardFilter.ActiveAvailable:
                     standards = standards.FilterActiveAvailableToStart();
                     break;
+                case StandardFilter.NotYetApproved:
+                    standards = standards.FilterNotYetApproved();
+                    break;
                 case StandardFilter.None:
                     break;
             }
@@ -26,19 +29,52 @@ namespace SFA.DAS.Courses.Data.Extensions
 
         private static IQueryable<Standard> FilterActiveAvailableToStart(this IQueryable<Standard> standards)
         {
-            var filteredStandards = standards.Where(ls => (ls.LarsStandard.LastDateStarts == null
-                                                          || ls.LarsStandard.LastDateStarts >= DateTime.UtcNow)
-                                                          && ls.LarsStandard.LastDateStarts != ls.LarsStandard.EffectiveFrom
-                                                          && ls.LarsStandard.EffectiveFrom <= DateTime.UtcNow);
+            var filteredStandards = standards
+                .FilterActive()
+                .IsAvailableToStart();
 
             return filteredStandards;
         }
 
         private static IQueryable<Standard> FilterActive(this IQueryable<Standard> standards)
         {
-            var filteredStandards = standards.Where(ls => ls.LarsStandard != null);
+            var filteredStandards = standards
+                .HasLarsStandard()
+                .IsLatestVersion()
+                .StatusIsOneOf("Approved for delivery");
 
             return filteredStandards;
+        }
+
+        private static IQueryable<Standard> FilterNotYetApproved(this IQueryable<Standard> standards)
+        {
+            var filteredStandards = standards.Where(ls => ls.LarsCode == 0)
+                                    .StatusIsOneOf("Proposal in development", "In development");
+                
+            return filteredStandards;
+        }
+
+        private static IQueryable<Standard> IsAvailableToStart(this IQueryable<Standard> standards)
+        {
+            return standards.Where(ls => (ls.LarsStandard.LastDateStarts == null
+                                                          || ls.LarsStandard.LastDateStarts >= DateTime.UtcNow)
+                                                          && ls.LarsStandard.LastDateStarts != ls.LarsStandard.EffectiveFrom
+                                                          && ls.LarsStandard.EffectiveFrom <= DateTime.UtcNow);
+        }
+
+        private static IQueryable<Standard> HasLarsStandard(this IQueryable<Standard> standards)
+        {
+            return standards.Where(ls => ls.LarsStandard != null);
+        }
+
+        private static IQueryable<Standard> StatusIsOneOf(this IQueryable<Standard> standards, params string[] statuses)
+        {
+            return standards.Where(ls => statuses.Contains(ls.Status, StringComparer.InvariantCultureIgnoreCase));
+        }
+
+        private static IQueryable<Standard> IsLatestVersion(this IQueryable<Standard> standards)
+        {
+            return standards.GroupBy(c => c.LarsCode).Select(c => c.OrderByDescending(x => x.Version).FirstOrDefault());
         }
     }
 }
