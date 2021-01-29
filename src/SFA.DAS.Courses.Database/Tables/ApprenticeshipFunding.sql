29410b4e5b1964e88d08f2b5a35ddf2b714a581f
dev4valtech <dev4valtech@gmail.com>
2021-01-29 10:28:06 +0000

    Revert ":boom: Updated table definitions to incorporate versioning change"
    This reverts commit 4c05d8e2e438394a84374b1a9d2cdb6fb3ec9641.

--- src/SFA.DAS.Courses.Database/Tables/ApprenticeshipFunding.sql
+++ src/SFA.DAS.Courses.Database/Tables/ApprenticeshipFunding.sql
@@ -1,15 +1,10 @@
 ﻿CREATE TABLE [dbo].[ApprenticeshipFunding]
 (
 	[Id] UNIQUEIDENTIFIER PRIMARY KEY,
-    [StandardUId] VARCHAR(20) NOT NULL,
-	[LarsCode] INT NOT NULL,
+	[StandardId] INT NOT NULL,
 	[EffectiveFrom] DATETIME NOT NULL,
 	[EffectiveTo] DATETIME NULL,
 	[MaxEmployerLevyCap] INT NOT NULL,
     [Duration] INT NOT NULL DEFAULT 0
 )
 GO
-
-CREATE NONCLUSTERED INDEX [IDX_ApprenticeshipFunding_StandardUId] ON [dbo].[ApprenticeshipFunding] ([StandardUId]) 
-INCLUDE ([Id], [LarsCode], [EffectiveFrom] , [EffectiveTo], [MaxEmployerLevyCap], [Duration]) WITH (ONLINE = ON) 
-GO 
--- src/SFA.DAS.Courses.Database/Tables/LarsStandard_Import.sql
+++ src/SFA.DAS.Courses.Database/Tables/LarsStandard_Import.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[LarsStandard_Import]
 (
-	[LarsCode] INT PRIMARY KEY,
+	[Id] UNIQUEIDENTIFIER PRIMARY KEY,
+	[StandardId] INT NOT NULL,
 	[Version] INT NOT NULL,
 	[EffectiveFrom] DATETIME NOT NULL,
 	[EffectiveTo] DATETIME NULL,
