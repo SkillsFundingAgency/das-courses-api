c5fc6f330662af7745d3ab1d3c7997811923caa0
dev4valtech <dev4valtech@gmail.com>
2021-01-29 10:28:06 +0000

    Revert ":boom: Updated table definitions to incorporate versioning change"
    This reverts commit 4c05d8e2e438394a84374b1a9d2cdb6fb3ec9641.

--- src/SFA.DAS.Courses.Database/Tables/Standard.sql
+++ src/SFA.DAS.Courses.Database/Tables/Standard.sql
@@ -1,8 +1,6 @@
 ﻿CREATE TABLE [dbo].[Standard]
 (
-	[StandardUId] VARCHAR(20) PRIMARY KEY,
-    [IfateReferenceNumber] VARCHAR(10) NOT NULL,
-    [LarsCode] INT NULL,
+	[Id] INT PRIMARY KEY,
 	[Title] VARCHAR(1000) NOT NULL,
 	[Level] INT NOT NULL,
 	[IntegratedDegree] VARCHAR(100) NULL,
@@ -18,18 +16,11 @@
 	[Behaviours] NVARCHAR(MAX) NULL, 
     [Duties] NVARCHAR(MAX) NULL, 
     [CoreAndOptions] BIT NOT NULL DEFAULT 0, 
-    [IntegratedApprenticeship] BIT NOT NULL DEFAULT 0
+    [IntegratedApprenticeship] BIT NOT NULL DEFAULT 0,
+    CONSTRAINT [AK_Standard_Column] UNIQUE ([Id])
 )
 GO
 
-CREATE NONCLUSTERED INDEX [IDX_Standard_LarsCode] ON [dbo].[Standard] (LarsCode) 
-INCLUDE ([StandardUId], [IfateReferenceNumber], [Title] , [Level], [IntegratedDegree], [OverviewOfRole], [RouteId], [Keywords], [TypicalJobTitles], [StandardPageUrl], [Version], [RegulatedBody], [Skills], [Knowledge], [Behaviours], [Duties], [CoreAndOptions], [IntegratedApprenticeship]) WITH (ONLINE = ON) 
-GO 
-
-CREATE NONCLUSTERED INDEX [IDX_Standard_IfateReferenceNumber] ON [dbo].[Standard] (IfateReferenceNumber) 
-INCLUDE ([StandardUId], [LarsCode], [Title] , [Level], [IntegratedDegree], [OverviewOfRole], [RouteId], [Keywords], [TypicalJobTitles], [StandardPageUrl], [Version], [RegulatedBody], [Skills], [Knowledge], [Behaviours], [Duties], [CoreAndOptions], [IntegratedApprenticeship]) WITH (ONLINE = ON) 
-GO 
-
 CREATE NONCLUSTERED INDEX [IDX_Standard_RouteId] ON [dbo].[Standard] (RouteId) 
-INCLUDE ([StandardUId], [IfateReferenceNumber], [LarsCode], [Title] , [Level], [IntegratedDegree], [OverviewOfRole], [Keywords], [TypicalJobTitles], [StandardPageUrl], [Version], [RegulatedBody], [Skills], [Knowledge], [Behaviours], [Duties], [CoreAndOptions], [IntegratedApprenticeship]) WITH (ONLINE = ON) 
+INCLUDE (Id,Title,[Level],  IntegratedDegree, OverviewOfRole,[Keywords],[TypicalJobTitles], [StandardPageUrl], [Version],[RegulatedBody],[Skills],[Knowledge],[Behaviours],[CoreAndOptions],[IntegratedApprenticeship]) WITH (ONLINE = ON) 
 GO 
