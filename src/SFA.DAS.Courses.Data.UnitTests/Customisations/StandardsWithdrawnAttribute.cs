20ebd5a6661e806cabd9c10fa6d4233aacfb860b
codewolf86 <chris@codewolfconsulting.com>
2021-02-02 13:13:48 +0000

    Update Unit Tests

---
+++ src/SFA.DAS.Courses.Data.UnitTests/Customisations/StandardsWithdrawnAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using AutoFixture;
+using AutoFixture.NUnit3;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.UnitTests.Customisations
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class StandardsWithdrawnAttribute : CustomizeAttribute
+    {
+        public override ICustomization GetCustomization(ParameterInfo parameter)
+        {
+            if (parameter == null)
+            {
+                throw new ArgumentNullException(nameof(parameter));
+            }
+
+            if (parameter.ParameterType.IsAssignableFrom(typeof(IEnumerable<Standard>)))
+            {
+                throw new ArgumentException(nameof(parameter));
+            }
+
+            return new StandardsWithdrawnCustomisation();
+        }
+    }
+
+    public class StandardsWithdrawnCustomisation : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Standard>(composer =>
+                composer.With(standard => standard.Status, "Withdrawn"));
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Data.UnitTests/Extensions/WhenNoFilterIsSpecified.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.Extensions;
+using SFA.DAS.Courses.Domain.Entities;
+using SFA.DAS.Courses.Domain.Search;
+namespace SFA.DAS.Courses.Data.UnitTests.Extensions
+{
+    public class WhenNoFilterIsSpecified
+    {
+        [Test]
+        public void Then_All_Standards_Are_Returned()
+        {
+            //Arrange
+            var sameDate = DateTime.UtcNow;
+            var standards = new List<Standard>
+            {
+                new Standard
+                {
+                    Title = "Not Yet Approved",
+                    Status = "In development",
+                },
+                new Standard
+                {
+                    Title = "Not Yet Approved",
+                    Status = "Proposal in development",
+                },
+                new Standard
+                {
+                    Title = "Withdrawn",
+                    Status = "Withdrawn",
+                },
+                new Standard
+                {
+                    Title = "Retired",
+                    Status = "Retired",
+                },
+                new Standard
+                {
+                    Title = "Available",
+                    ApprenticeshipFunding = new List<ApprenticeshipFunding>(),
+                    LarsStandard = new LarsStandard
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
+                        LastDateStarts = DateTime.UtcNow.AddDays(-1)
+                    },
+                    Status = "Approved for delivery"
+                },
+                new Standard
+                {
+                    Title = "Available",
+                    ApprenticeshipFunding = new List<ApprenticeshipFunding>(),
+                    LarsStandard = new LarsStandard
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddMonths(1),
+                        LastDateStarts = null
+                    },
+                    Status = "Approved for delivery"
+                },
+                new Standard
+                {
+                    Title = "Available",
+                    ApprenticeshipFunding = new List<ApprenticeshipFunding>(),
+                    LarsStandard =
+                        new LarsStandard
+                        {
+                            EffectiveFrom = DateTime.UtcNow.AddMonths(-2),
+                            LastDateStarts = DateTime.UtcNow.AddMonths(-1)
+                        },
+                    Status = "Approved for delivery"
+                }
+            }.AsQueryable();
+
+            //Act
+            var actual = standards.FilterStandards(StandardFilter.None);
+
+            //Assert
+            actual.Count().Should().Be(7);
+        }
+    }
+}
