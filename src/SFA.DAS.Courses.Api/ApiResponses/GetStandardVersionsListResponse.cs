83d7df7fc8ea0aca49fa060ae6301cd9ae5f1844
codewolf86 <chris@codewolfconsulting.com>
2021-02-05 10:24:41 +0000

    Reverse Naming Switch

--- src/SFA.DAS.Courses.Api/ApiResponses/GetStandardVersionsListResponse.cs
+++ src/SFA.DAS.Courses.Api/ApiResponses/GetStandardVersionsListResponse.cs
@@ -2,10 +2,8 @@
 
 namespace SFA.DAS.Courses.Api.ApiResponses
 {
-    public class GetStandardsListResponse
+    public class GetStandardVersionsListResponse
     {
-        public int Total { get; set; }
-        public int TotalFiltered { get; set; }
         public IEnumerable<GetStandardResponse> Standards { get; set; }
     }
 }
