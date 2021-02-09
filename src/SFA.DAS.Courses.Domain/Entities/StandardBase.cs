a90703f4782c2761d8b1fe8b7db890fbb0a46ef2
dev4valtech <dev4valtech@gmail.com>
2021-02-09 14:50:04 +0000

    :sparkles: SV-388 Added more fields to standard

--- src/SFA.DAS.Courses.Domain/Entities/StandardBase.cs
+++ src/SFA.DAS.Courses.Domain/Entities/StandardBase.cs
@@ -9,16 +9,28 @@ namespace SFA.DAS.Courses.Domain.Entities
         public string IfateReferenceNumber { get; set; }
         public int LarsCode { get; set; }
         public string Status { get; set; }
+        public DateTime? EarliestStartDate { get; set; }
+        public DateTime? LatestStartDate { get; set; }
+        public DateTime? LatestEndDate { get; set; }
         public string Title { get; set; }
         public int Level { get; set; }
+        public int TypicalDuration { get; set; }
+        public int MaxFunding { get; set; }
         public decimal Version { get; set; }
         public string OverviewOfRole { get; set; }
         public virtual Sector Sector {get; set; }
         public Guid RouteId { get; set; }
+        public string AssessmentPlanUrl { get; set; }
         public string Keywords { get; set; }
         public string TypicalJobTitles { get; set; }
         public string StandardPageUrl { get; set; }
         public string IntegratedDegree { get; set; }
+        public DateTime? ApprovedForDelivery { get; set; }
+        public string TrailBlazerContact { get; set; }
+        public string EqaProviderName { get; set; }
+        public string EqaProviderContactName { get; set; }
+        public string EqaProviderContactEmail { get; set; }
+        public string EqaProviderWebLink { get; set; }
         public string RegulatedBody { get; set; }
         public virtual LarsStandard LarsStandard { get; set; }
         public virtual ICollection<ApprenticeshipFunding> ApprenticeshipFunding { get; set; }
