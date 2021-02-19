399e8e40bd51b1a936356378508b5688a8bda8d7
dev4valtech <dev4valtech@gmail.com>
2021-02-19 12:41:01 +0000

    :bug: In active filter include retired standard with distinct larscode

--- src/SFA.DAS.Courses.Data.UnitTests/Repository/StandardRepository/WhenGettingCount.cs
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/StandardRepository/WhenGettingCount.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
@@ -61,9 +62,44 @@ namespace SFA.DAS.Courses.Data.UnitTests.Repository.StandardRepository
 
             var count = await repository.Count(StandardFilter.Active);
 
-            
             count.Should().Be(activeValidStandards.Count + activeInvalidStandards.Count + retiredStandards.Count);
         }
+
+        [Test, RecursiveMoqAutoData]
+        public async Task Then_Gets_Count_From_Context_Of_Active_Standards_Including_Retired_With_Distinct_LarsCode(
+            [StandardsAreLarsValid] List<Standard> activeValidStandards,
+            [StandardsNotLarsValid] List<Standard> activeInvalidStandards,
+            [StandardsNotYetApproved] List<Standard> notYetApprovedStandards,
+            [StandardsWithdrawn] List<Standard> withdrawnStandards,
+            [StandardsRetired] List<Standard> retiredStandards,
+            [Frozen] Mock<ICoursesDataContext> mockDataContext,
+            Data.Repository.StandardRepository repository)
+        {
+            var allStandards = new List<Standard>();
+            allStandards.AddRange(activeValidStandards);
+            allStandards.AddRange(activeInvalidStandards);
+            allStandards.AddRange(notYetApprovedStandards);
+            allStandards.AddRange(withdrawnStandards);
+            allStandards.AddRange(retiredStandards);
+
+            //set up active version 
+            var newActiveVersion = activeValidStandards.First();
+            newActiveVersion.IfateReferenceNumber = "ST0001";
+            newActiveVersion.Version = 2;
+            newActiveVersion.LarsCode = 100002;
+
+            //add a retired version to have same IfateReferenceNumber and different LarsCode
+            allStandards.Add(activeValidStandards.Select(x => new Standard { IfateReferenceNumber = x.IfateReferenceNumber, Status = "Retired", LarsCode = 100001, Version = 1, LarsStandard = newActiveVersion.LarsStandard }).First());
+
+            mockDataContext
+                .Setup(context => context.Standards)
+                .ReturnsDbSet(allStandards);
+
+            var count = await repository.Count(StandardFilter.Active);
+
+            count.Should().Be(activeValidStandards.Count + activeInvalidStandards.Count + retiredStandards.Count + 1);
+        }
+
 
         [Test, RecursiveMoqAutoData]
         public async Task Then_Gets_Count_From_Context_Of_NotYetApproved_Standards(
