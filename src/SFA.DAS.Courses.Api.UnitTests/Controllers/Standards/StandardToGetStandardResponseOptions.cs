fdc11c4a5fb75a02608258e10976e047be71b00a
dev4valtech <dev4valtech@gmail.com>
2021-02-22 14:24:33 +0000

    :hammer: Reduced the payload of GetList and GetByLarsCode

---
+++ src/SFA.DAS.Courses.Api.UnitTests/Controllers/Standards/StandardToGetStandardResponseOptions.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions.Equivalency;
+
+namespace SFA.DAS.Courses.Api.UnitTests.Controllers.Standards
+{
+    public static class StandardToGetStandardResponseOptions
+    {
+        public static EquivalencyAssertionOptions<Domain.Courses.Standard> Exclusions(EquivalencyAssertionOptions<Domain.Courses.Standard> config) => config
+            .Excluding(s => s.AssessmentPlanUrl)
+            .Excluding(s => s.VersionDetail)
+            .Excluding(s => s.EqaProvider)
+            .Excluding(s => s.Options)
+            .Excluding(s => s.TrailBlazerContact);
+    }
+}
