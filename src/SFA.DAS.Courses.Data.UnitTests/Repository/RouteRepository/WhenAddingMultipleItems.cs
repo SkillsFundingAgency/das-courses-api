767883abd0677b51900e30c547dfebfad7b79e60
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-03 16:43:52 +0000

    FAT2-412 - Add Route Repository
    Added methods for remove all, add many and get all

---
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/RouteRepository/WhenAddingMultipleItems.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.UnitTests.Repository.RouteRepository
+{
+    public class WhenAddingMultipleItems
+    {
+        private Mock<ICoursesDataContext> _coursesDataContext;
+        private Data.Repository.RouteRepository _routeRepository;
+        private List<Route> _routes;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _routes = new List<Route>
+            {
+                new Route
+                {
+                    Id = 1,
+                    Name = "Test Route"
+                },
+                new Route
+                {
+                    Id = 2,
+                    Name = "Test Route 2"
+                }
+            };
+
+            _coursesDataContext = new Mock<ICoursesDataContext>();
+            _coursesDataContext.Setup(x => x.Routes).ReturnsDbSet(new List<Route>());
+            _routeRepository = new Data.Repository.RouteRepository(_coursesDataContext.Object);
+        }
+
+        [Test]
+        public async Task Then_The_Routes_Are_Added()
+        {
+            //Act
+            await _routeRepository.InsertMany(_routes);
+            
+            //Assert
+            _coursesDataContext.Verify(x=>x.Routes.AddRangeAsync(_routes, It.IsAny<CancellationToken>()), Times.Once);
+            _coursesDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/RouteRepository/WhenGettingRoutes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.UnitTests.Repository.RouteRepository
+{
+    public class WhenGettingRoutes
+    {
+        private Mock<ICoursesDataContext> _coursesDataContext;
+        private Data.Repository.RouteRepository _routeRepository;
+        private List<Route> _routes;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _routes = new List<Route>
+            {
+                new Route
+                {
+                    Id = 1,
+                    Name = "Test Route"
+                },
+                new Route
+                {
+                    Id = 2,
+                    Name = "Test Route 2"
+                }
+            };
+            
+            _coursesDataContext = new Mock<ICoursesDataContext>();
+            _coursesDataContext.Setup(x => x.Routes).ReturnsDbSet(_routes);
+            
+            _routeRepository = new Data.Repository.RouteRepository(_coursesDataContext.Object);
+        }
+        
+        [Test]
+        public async Task Then_The_Routes_Are_Returned()
+        {
+            //Act
+            var actual = await _routeRepository.GetAll();
+            
+            //Assert
+            Assert.IsNotNull(actual);
+            actual.Should().BeEquivalentTo(_routes);
+        }
+    }
+}
