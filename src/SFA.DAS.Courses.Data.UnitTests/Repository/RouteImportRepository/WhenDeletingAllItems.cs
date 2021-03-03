47b6eafbbc45cd993f050c5168070bc9b27f7d51
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-03 16:43:31 +0000

    FAT2-412 - Add route import repository
    Added methods for remove all, add many and get all

---
+++ src/SFA.DAS.Courses.Data.UnitTests/Repository/RouteImportRepository/WhenDeletingAllItems.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.UnitTests.Repository.RouteImportRepository
+{
+    public class WhenDeletingAllItems
+    {
+        private Mock<ICoursesDataContext> _coursesDataContext;
+        private Data.Repository.RouteImportRepository _routeImportRepository;
+        private List<RouteImport> _routes;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _routes = new List<RouteImport>
+            {
+                new RouteImport
+                {
+                    Id = 1,
+                    Name = "Test Route"
+                },
+                new RouteImport
+                {
+                    Id = 2,
+                    Name = "Test Route 2"
+                }
+            };
+            
+            _coursesDataContext = new Mock<ICoursesDataContext>();
+            _coursesDataContext.Setup(x => x.RoutesImport).ReturnsDbSet(_routes);
+            
+
+            _routeImportRepository = new Data.Repository.RouteImportRepository(_coursesDataContext.Object);
+        }
+
+        [Test]
+        public void Then_The_RoutesImport_Items_Are_Removed()
+        {
+            //Act
+            _routeImportRepository.DeleteAll();
+            
+            //Assert
+            _coursesDataContext.Verify(x=>x.RoutesImport.RemoveRange(_coursesDataContext.Object.RoutesImport), Times.Once);
+            _coursesDataContext.Verify(x=>x.SaveChanges(), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.Courses.Data/Repository/RouteImportRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Courses.Domain.Entities;
+
+namespace SFA.DAS.Courses.Data.Repository
+{
+    public class RouteImportRepository
+    {
+        private readonly ICoursesDataContext _coursesDataContext;
+
+        public RouteImportRepository(ICoursesDataContext coursesDataContext)
+        {
+            _coursesDataContext = coursesDataContext;
+        }
+
+        public async Task InsertMany(IEnumerable<RouteImport> routes)
+        {
+            await _coursesDataContext.RoutesImport.AddRangeAsync(routes);
+            _coursesDataContext.SaveChanges();
+        }
+
+        public void DeleteAll()
+        {
+            _coursesDataContext.RoutesImport.RemoveRange(_coursesDataContext.RoutesImport);
+            _coursesDataContext.SaveChanges();
+        }
+
+        public async Task<IEnumerable<RouteImport>> GetAll()
+        {
+            return await _coursesDataContext.RoutesImport.ToListAsync();
+        }
+    }
+}
