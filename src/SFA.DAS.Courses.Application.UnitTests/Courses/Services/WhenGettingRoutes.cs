d69d11d2270359b4241a4019b047a8e5310a6677
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-03 18:16:44 +0000

    FAT2-412 - Add RouteService
    Gets all routes and maps to domain model from entity

---
+++ src/SFA.DAS.Courses.Application.UnitTests/Courses/Services/WhenGettingRoutes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Application.Courses.Services;
+using SFA.DAS.Courses.Domain.Entities;
+using SFA.DAS.Courses.Domain.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Courses.Application.UnitTests.Courses.Services
+{
+    public class WhenGettingRoutes
+    {
+        [Test, RecursiveMoqAutoData]
+        public async Task Then_Gets_Sectors_From_Repository(
+            List<Route> routeEntities,
+            [Frozen] Mock<IRouteRepository> routeRepository,
+            RouteService service)
+        {
+            routeRepository
+                .Setup(repository => repository.GetAll())
+                .ReturnsAsync(routeEntities);
+
+            var sectors = (await service.GetRoutes()).ToList();
+
+            sectors.Should().BeEquivalentTo(routeEntities, options=> options
+                .Excluding(c=>c.Standards)
+            );
+
+        }
+    }
+}
