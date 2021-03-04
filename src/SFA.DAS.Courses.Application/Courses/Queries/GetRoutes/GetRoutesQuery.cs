803acba7a2706f7926a93e23872b681f6e4404fe
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-03-04 09:25:37 +0000

    FAT2-412 - Add handler for getting routes

---
+++ src/SFA.DAS.Courses.Application/Courses/Queries/GetRoutes/GetRoutesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Courses.Application.Courses.Queries.GetRoutes
+{
+    public class GetRoutesQuery : IRequest<GetRoutesQueryResult>
+    {
+        
+    }
+}
---
+++ src/SFA.DAS.Courses.Application/Courses/Queries/GetRoutes/GetRoutesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Courses.Domain.Interfaces;
+
+namespace SFA.DAS.Courses.Application.Courses.Queries.GetRoutes
+{
+    public class GetRoutesQueryHandler : IRequestHandler<GetRoutesQuery, GetRoutesQueryResult>
+    {
+        private readonly IRouteService _service;
+
+        public GetRoutesQueryHandler (IRouteService service)
+        {
+            _service = service;
+        }
+        public async Task<GetRoutesQueryResult> Handle(GetRoutesQuery request, CancellationToken cancellationToken)
+        {
+            var routes = await _service.GetRoutes();
+            
+            return new GetRoutesQueryResult
+            {
+                Routes = routes
+            };
+        }
+    }
+}
