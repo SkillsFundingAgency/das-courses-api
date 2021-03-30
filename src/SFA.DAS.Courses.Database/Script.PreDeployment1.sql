e830df500d2fa28680d6e71d4b471b6ce50a823a
Ben Stone <ben1.stone@education.gov.uk>
2021-03-30 13:06:41 +0100

    SV-568 Add pre deployment script to clear Stand and Standard_Import tables

---
+++ src/SFA.DAS.Courses.Database/Script.PreDeployment1.sql
@@ -0,0 +1,9 @@
+﻿IF (EXISTS(SELECT * FROM [dbo].[Standard]))  
+BEGIN  
+    DELETE FROM [dbo].[Standard]  
+END  
+
+IF (EXISTS(SELECT * FROM [dbo].[Standard_Import]))  
+BEGIN  
+    DELETE FROM [dbo].[Standard_Import]  
+END  
