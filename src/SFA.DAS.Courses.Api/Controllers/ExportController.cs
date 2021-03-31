70ea1e3756ed4b53a9cf6e70d6664cd7da5530e5
CodeWolf86 <54770494+CodeWolf86@users.noreply.github.com>
2021-03-31 15:49:32 +0100

    Add new Standards Export Endpoint (#89)

---
+++ src/SFA.DAS.Courses.Api/Controllers/ExportController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Courses.Api.ApiResponses;
+using SFA.DAS.Courses.Application.Courses.Queries.GetStandardsList;
+using SFA.DAS.Courses.Domain.Search;
+
+namespace SFA.DAS.Courses.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("/ops/export")]
+    public class ExportController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ExportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            var queryResult = await _mediator.Send(new GetStandardsListQuery
+            {
+                Filter = StandardFilter.None
+            });
+
+            var response = new GetStandardsExportResponse
+            {
+                Standards = queryResult.Standards.Select(standard => (GetStandardDetailResponse)standard)
+            };
+
+            return Ok(response);
+        }
+    }
+}
