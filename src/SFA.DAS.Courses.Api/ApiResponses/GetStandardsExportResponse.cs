b3257b07c90981a105ce887f942de42af0015d03
codewolf86 <chris@codewolfconsulting.com>
2021-03-31 15:36:35 +0100

    Add new Standards Export Endpoint

---
+++ src/SFA.DAS.Courses.Api/ApiResponses/GetStandardsExportResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Courses.Api.ApiResponses
+{
+    public class GetStandardsExportResponse
+    {
+        public IEnumerable<GetStandardDetailResponse> Standards { get; set; }
+    }
+}
