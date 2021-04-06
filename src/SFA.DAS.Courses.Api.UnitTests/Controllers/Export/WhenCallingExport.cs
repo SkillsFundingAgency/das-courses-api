02225695edd48e819d94ee5f7b86890eb8887be6
CodeWolf86 <54770494+CodeWolf86@users.noreply.github.com>
2021-04-06 08:59:28 +0100

    SV-171 Add new Standards Export Endpoint  (#90)
    * Add new Standards Export Endpoint (#89)
    
    * Add Unit Test

---
+++ src/SFA.DAS.Courses.Api.UnitTests/Controllers/Export/WhenCallingExport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Courses.Api.ApiResponses;
+using SFA.DAS.Courses.Api.Controllers;
+using SFA.DAS.Courses.Application.Courses.Queries.GetStandardsList;
+using SFA.DAS.Courses.Domain.Search;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Courses.Api.UnitTests.Controllers.Import
+{
+    public class WhenCallingExport
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Sends_GetStandardsListQuery_To_Mediator(
+            [Frozen] Mock<IMediator> mockMediator,
+            GetStandardsListResult queryResult,
+            [Greedy] ExportController controller)
+        {
+            mockMediator.Setup(mediator => mediator.Send(
+                It.Is<GetStandardsListQuery>(query => query.Filter.Equals(StandardFilter.None)),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(queryResult);
+
+            var controllerResult = await controller.Index() as ObjectResult;
+
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            mockMediator.Verify(x => x.Send(It.IsAny<GetStandardsListQuery>(), It.IsAny<CancellationToken>()), Times.Once);
+            var model = controllerResult.Value as GetStandardsExportResponse;
+            model.Standards.Should().BeEquivalentTo(queryResult.Standards);
+        }
+    }
+}
