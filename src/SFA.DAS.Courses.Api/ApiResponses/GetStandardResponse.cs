2d0fc9bf311181f98ff495b732496bb16398b845
Matt Daniels <matt@techtoniq.com>
2021-05-19 11:09:45 +0100

    EPAChanged field added to API output.

--- src/SFA.DAS.Courses.Api/ApiResponses/GetStandardResponse.cs
+++ src/SFA.DAS.Courses.Api/ApiResponses/GetStandardResponse.cs
@@ -37,6 +37,7 @@ namespace SFA.DAS.Courses.Api.ApiResponses
         public List<string> CoreDuties { get; set; }
         public bool IntegratedApprenticeship { get ; set ; }
         public int SectorCode { get ; set ; }
+        public bool EPAChanged { get; set; }
 
         public static implicit operator GetStandardResponse(Standard source)
         {
@@ -69,7 +70,8 @@ namespace SFA.DAS.Courses.Api.ApiResponses
                 CoreAndOptions = source.CoreAndOptions,
                 CoreDuties = source.CoreDuties,
                 IntegratedApprenticeship = source.IntegratedApprenticeship,
-                SectorCode = source.SectorCode
+                SectorCode = source.SectorCode,
+                EPAChanged = source.EPAChanged
             };
         }
     }
