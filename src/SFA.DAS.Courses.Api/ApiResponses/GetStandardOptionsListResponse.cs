bb124079af599de946fa8d86eaa0db510508628b
codewolf86 <chris@codewolfconsulting.com>
2021-08-23 14:10:58 +0100

    Remove un-used files

--- src/SFA.DAS.Courses.Api/ApiResponses/GetStandardOptionsListResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.Courses.Api.ApiResponses
-{
-    public class GetStandardOptionsListResponse
-    {
-        public IEnumerable<GetStandardOptionsResponse> StandardOptions { get; set; }
-    }
-}
