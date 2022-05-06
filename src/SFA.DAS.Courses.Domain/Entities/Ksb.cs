41cc9e12dcfa9be2905dc32d81f323dc06510ba2
Ben Arnold <ben@arnoldfamily.me.uk>
2022-05-06 11:11:03 +0100

    Encapsulate Ksb

---
+++ src/SFA.DAS.Courses.Domain/Entities/Ksb.cs
@@ -0,0 +1,33 @@
+﻿namespace SFA.DAS.Courses.Domain.Entities
+{
+    public enum KsbType
+    {
+        Knowledge = 1,
+        Skill = 2,
+        Behaviour = 3,
+    }
+
+    public class Ksb
+    {
+        public KsbType Type { get; }
+        public string Key { get; }
+        public string Detail { get; }
+
+        private Ksb()
+        {
+            /* Entity Framework */
+        }
+
+        public static Ksb Knowledge(int index, string detail)
+            => new Ksb(KsbType.Knowledge, index, detail);
+
+        public static Ksb Skill(int index, string detail)
+            => new Ksb(KsbType.Skill, index, detail);
+
+        public static Ksb Behaviour(int index, string detail)
+            => new Ksb(KsbType.Behaviour, index, detail);
+
+        private Ksb(KsbType type, int index, string detail)
+            => (Type, Key, Detail) = (type, $"{type.ToString()[0]}{index}", detail);
+    }
+}
--- src/SFA.DAS.Courses.Domain/Entities/StandardOption.cs
+++ src/SFA.DAS.Courses.Domain/Entities/StandardOption.cs
@@ -13,27 +13,4 @@ namespace SFA.DAS.Courses.Domain.Entities
         public List<string> Behaviours => AllKsbs?.Where(x => x.Type == KsbType.Behaviour).Select(x => x.Detail).ToList();
         public List<Ksb> AllKsbs { get; set; }
     }
-
-    public class Ksb
-    {
-        public static Ksb Knowledge(int index, string detail)
-            => new Ksb { Type = KsbType.Knowledge, Key = $"K{index}", Detail = detail };
-        
-        public static Ksb Skill(int index, string detail)
-            => new Ksb { Type = KsbType.Skill, Key = $"S{index}", Detail = detail };
-        
-        public static Ksb Behaviour(int index, string detail)
-            => new Ksb { Type = KsbType.Behaviour, Key = $"B{index}", Detail = detail };
-
-        public KsbType Type { get; set; }
-        public string Key { get; set; }
-        public string Detail { get; set; }
-    }
-
-    public enum KsbType
-    {
-        Knowledge = 1,
-        Skill = 2,
-        Behaviour = 3,
-    }
 }
