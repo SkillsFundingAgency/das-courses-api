0d30930838e325237515341305c2afa2dfc56a60
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-07-26 12:57:43 +0100

    FAI-583 - Update LarsBasePageURL
    Update LarsBasePageUrl and download page url due to update from LARS

--- src/SFA.DAS.Courses.Domain/Configuration/Constants.cs
+++ src/SFA.DAS.Courses.Domain/Configuration/Constants.cs
@@ -4,8 +4,8 @@
     {
         public static string InstituteOfApprenticeshipsStandardsUrl => "https://www.instituteforapprenticeships.org/api/apprenticeshipstandards/";
         public static string QualificationSectorSubjectAreaUrl => "https://qualification-sector-subject-area.register.gov.uk/";
-        public static string LarsBasePageUrl => "https://findalearningaimbeta.fasst.org.uk";
-        public static string LarsDownloadPageUrl => $"{LarsBasePageUrl}/DownloadData";
+        public static string LarsBasePageUrl => "https://submit-learner-data.service.gov.uk";
+        public static string LarsDownloadPageUrl => $"{LarsBasePageUrl}/find-a-learning-aim/DownloadData";
         public static string LarsStandardsFileName => "Standard.csv";
         public static string LarsApprenticeshipFundingFileName => "ApprenticeshipFunding.csv";
         public static string LarsSectorSubjectAreaTier2FileName => "SectorSubjectAreaTier2.csv";
