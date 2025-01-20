4c90e1976aec707d86fe02f3ee9b1a00a2f5019d
Chris Woodcock <chris@humesoftware.com>
2025-01-20 12:08:09 +0000

    Fixed unit test

--- src/SFA.DAS.Courses.Api.UnitTests/Controllers/Import/WhenCallingStandardsImportUrl.cs
+++ src/SFA.DAS.Courses.Api.UnitTests/Controllers/Import/WhenCallingStandardsImportUrl.cs
@@ -1,6 +1,5 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using SFA.DAS.Courses.Api.Controllers;
@@ -12,13 +11,13 @@ namespace SFA.DAS.Courses.Api.UnitTests.Controllers.Import
     public class WhenCallingStandardsImportUrl
     {
         [Test, MoqAutoData]
-        public void Then_Returns_Ok_Result_With_StandardsImportUrl(
+        public void Then_Returns_Content_Result_With_StandardsImportUrl(
             [Greedy] DataLoadController controller)
         {
-            var controllerResult = controller.StandardsImportUrl() as OkObjectResult;
+            var controllerResult = controller.StandardsImportUrl() as ContentResult;
 
-            controllerResult.Value.Should().Be(Constants.InstituteOfApprenticeshipsStandardsUrl);
-            controllerResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+            controllerResult.Content.Should().Be(Constants.InstituteOfApprenticeshipsStandardsUrl);
+            controllerResult.StatusCode.Should().BeNull();
         }
     }
 }
