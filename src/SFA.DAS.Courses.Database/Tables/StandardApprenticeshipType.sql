529ba8f930ae4e071f5100d619924d2408b44ef4
Deven Shah <devenshah1974@gmail.com>
2025-03-03 14:03:57 +0000

    CSP-1931 Increased field length

--- src/SFA.DAS.Courses.Database/Tables/StandardApprenticeshipType.sql
+++ src/SFA.DAS.Courses.Database/Tables/StandardApprenticeshipType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[StandardApprenticeshipType]
 (
     [IfateReferenceNumber] VARCHAR(10) NOT NULL,
-    [ApprenticeshipType] VARCHAR(20) NOT NULL,
+    [ApprenticeshipType] VARCHAR(50) NOT NULL,
     CONSTRAINT [PK_StandardApprenticeshipType] PRIMARY KEY ([IfateReferenceNumber])
 )
