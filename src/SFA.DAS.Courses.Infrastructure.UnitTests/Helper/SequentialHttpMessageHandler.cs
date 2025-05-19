86d98a1e89cc70a878f0f09e4ddda23652efec7a
Chris Woodcock <chris@humesoftware.com>
2025-05-19 09:10:44 +0100

    Disabled slack upload when no bot token specified

---
+++ src/SFA.DAS.Courses.Infrastructure.UnitTests/Helper/SequentialHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Courses.Infrastructure.UnitTests.Helper
+{
+    public class SequentialHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Queue<HttpResponseMessage> _responses;
+
+        public SequentialHttpMessageHandler(IEnumerable<HttpResponseMessage> responses)
+        {
+            _responses = new Queue<HttpResponseMessage>(responses);
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_responses.Dequeue());
+        }
+    }
+
+}
