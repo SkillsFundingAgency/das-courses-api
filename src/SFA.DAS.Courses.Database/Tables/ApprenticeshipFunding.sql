7e2f73f314c08d17d4c04fee694cfc94562f1930
Deven Shah <devenshah1974@gmail.com>
2025-05-27 15:38:44 +0100

    CSP-2019 Load additional funding fields from lars

--- src/SFA.DAS.Courses.Database/Tables/ApprenticeshipFunding.sql
+++ src/SFA.DAS.Courses.Database/Tables/ApprenticeshipFunding.sql
@@ -1,11 +1,18 @@
 ﻿CREATE TABLE [dbo].[ApprenticeshipFunding]
 (
     [Id] UNIQUEIDENTIFIER PRIMARY KEY,
-	[StandardUId] VARCHAR(20) NOT NULL,
-	[EffectiveFrom] DATETIME NOT NULL,
-	[EffectiveTo] DATETIME NULL,
-	[MaxEmployerLevyCap] INT NOT NULL,
-    [Duration] INT NOT NULL DEFAULT 0
+    [StandardUId] VARCHAR(20) NOT NULL,
+    [EffectiveFrom] DATETIME NOT NULL,
+    [EffectiveTo] DATETIME NULL,
+    [MaxEmployerLevyCap] INT NOT NULL,
+    [Duration] INT NOT NULL DEFAULT 0,
+    [Incentive1618] int,
+    [ProviderAdditionalPayment1618] int,
+    [EmployerAdditionalPayment1618] int,
+    [CareLeaverAdditionalPayment] int,
+    [FoundationAppFirstEmpPayment] int,
+    [FoundationAppSecondEmpPayment] int,
+    [FoundationAppThirdEmpPayment] int
 )
 GO
 
