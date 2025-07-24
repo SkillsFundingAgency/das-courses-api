842d617b94edefb2ce7091a05c675d0b7a2006ff
Deven Shah <devenshah1974@gmail.com>
2025-07-24 09:42:17 +0100

    CSP-2193 Added related occupation to reverse transformation

---
+++ src/SFA.DAS.Courses.Domain.UnitTests/Entities/WhenCastingFromStandardEntityToStandardImport.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Courses.Domain.Entities;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Courses.Domain.UnitTests.Entities;
+public class WhenCastingFromStandardEntityToStandardImport
+{
+    [Test, RecursiveMoqAutoData]
+    public void Then_StandardEntity_Can_Be_Cast_To_StandardImport(
+        Standard standardEntity)
+    {
+        // Act
+        StandardImport standardImport = standardEntity;
+        // Assert
+        standardImport.Should().BeEquivalentTo(standardEntity, options => options
+            .Excluding(s => s.StandardUId)
+            .Excluding(s => s.LarsStandard)
+            .Excluding(s => s.ApprenticeshipFunding)
+            .Excluding(s => s.SearchScore));
+    }
+}
