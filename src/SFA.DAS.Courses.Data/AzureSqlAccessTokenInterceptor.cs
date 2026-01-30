70ef8c6b5c6167dfadb625a4c3a9e0cc559db522
Chris Woodcock <chris@humesoftware.com>
2026-01-30 17:48:18 +0000

    Using async interceptor to get azure access token

---
+++ src/SFA.DAS.Courses.Data/AzureSqlAccessTokenInterceptor.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace SFA.DAS.Courses.Data
+{
+    public sealed class AzureSqlAccessTokenInterceptor : DbConnectionInterceptor
+    {
+        private const string AzureResource = "https://database.windows.net/";
+        private readonly AzureServiceTokenProvider _tokenProvider;
+
+        public AzureSqlAccessTokenInterceptor(AzureServiceTokenProvider tokenProvider)
+            => _tokenProvider = tokenProvider;
+
+        public override async ValueTask<InterceptionResult> ConnectionOpeningAsync(
+            DbConnection connection,
+            ConnectionEventData eventData,
+            InterceptionResult result,
+            CancellationToken cancellationToken = default)
+        {
+            if (connection is SqlConnection sql && string.IsNullOrEmpty(sql.AccessToken))
+            {
+                sql.AccessToken = await _tokenProvider
+                    .GetAccessTokenAsync(AzureResource)
+                    .ConfigureAwait(false);
+            }
+
+            return result;
+        }
+    }
+
+}
